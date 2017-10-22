de869c02e2de7a26dabd85c6e93c7999c8bc1081
katsuraku <kirsten.jones@ft.com>
2017-10-22 10:51:11 +0100

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+let path = require('path');
+let LiveReloadPlugin = require('webpack-livereload-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/dist')
+  },
+  context: __dirname,
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '*']
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['react', 'es2015']
+      }
+    },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      // space for more plugins - sure to get some soon
+    }),
+    new LiveReloadPlugin({appendScriptTag: true})
+  ]
+};
