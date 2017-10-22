3129286782a87ea9aa409a93baa78c25e1718421
katsuraku <kirsten.jones@ft.com>
2017-10-22 10:53:32 +0100

    Use express

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//serve up static files
+app.use(express.static(path.resolve(__dirname, '..', 'client')));
+app.use(express.static(path.resolve(__dirname, '..', 'node_modules')));
+
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+
+// handle every other route with index.html, which will contain
+// a script tag to the app's js file(s).
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, '..', 'client', 'index.html'))
+});
+
+//listen on port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port 3000");
+});
